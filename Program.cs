a42a004477c1658c10dd1b73c4c36069fb95eb04
Kumari <2064588@cognizant.com>
2022-08-29 20:46:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DLL2;
+
+namespace BLL1
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            DLL2.Class1 dal = new DLL2.Class1();
+            dal.RetrieveData();
+
+
+        }
+
+    }
+}
